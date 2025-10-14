e2ccf192c93e0f1ac65d7689e1ebd308e1ed528c
Shany Baruch <baruchshany@gmail.com>
2025-10-14 15:06:14 +0300

    Repair app appsus

---
+++ apps/mail/cmps/MailHeader.jsx
@@ -0,0 +1,26 @@
+const { Link, NavLink } = ReactRouterDOM
+
+
+export function MailHeader() {
+    return (
+        <section className="mail-header flex space-between">
+
+            <section className="side-header grid">
+                <a class="menu fa-solid fa-bars"></a>
+                <Link className="flex align-center" to="/">
+                    <img className="logo-img" src="assets/css/img/logo.png" alt="logo" />
+                    <h3 className="logo-title">MisterEmail</h3>
+                </Link>
+            </section>
+
+            <section className="search-line">
+                <input type="text" placeholder="Search mail" />
+            </section>
+
+            <section className="flex align-center">
+                <a class="settings fa-solid fa-gear"></a>
+            </section>
+
+        </section>
+    )
+}
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,22 @@
+import { MailHeader } from "../cmps/MailHeader.jsx"
+
+const { Link, NavLink } = ReactRouterDOM
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
+    return (
+        <section className="mail-app container">
+            <section className="header grid">
+                <MailHeader />
+
+                {/* <MailFilter onSetFilterBy={onSetFilterBy} defaultFilter={filterBy} />
+                <Link className="add-mail" to="/mail/edit">Add mail</Link>
+            <MailList
+                mails={mails}
+                onRemoveMail={onRemoveMail}
+                /> */}
+
+                </section>
+        </section>
+    )
 }
 
